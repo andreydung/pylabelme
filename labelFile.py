ae02de8f567b630cb5f8a82126611dc82e0bf5e4
Dzung Nguyen <dzungng89@gmail.com>
2014-06-12 13:48:51 -0500

    Faster image writing

--- labelFile.py
+++ labelFile.py
@@ -19,7 +19,6 @@
 
 import json
 import os.path
-import cv2
 import numpy as np
 
 from PyQt4.QtGui import *
@@ -28,91 +27,108 @@ from PyQt4.QtCore import *
 from base64 import b64encode, b64decode
 
 class LabelFileError(Exception):
-    pass
+	pass
 
 class LabelFile(object):
-    suffix = '.lif'
+	suffix = '.lif'
 
-    def __init__(self, filename=None):
-        self.shapes = ()
-        self.imagePath = None
-        self.imageData = None
-        if filename is not None:
-            self.load(filename)
+	def __init__(self, filename=None):
+		self.shapes = ()
+		self.imagePath = None
+		self.imageData = None
+		if filename is not None:
+			self.load(filename)
 
-    def load(self, filename):
-        try:
-            with open(filename, 'rb') as f:
-                data = json.load(f)
-                imagePath = data['imagePath']
-                imageData = b64decode(data['imageData'])
-                lineColor = data['lineColor']
-                fillColor = data['fillColor']
-                shapes = ((s['label'], s['points'], s['line_color'], s['fill_color'])\
-                        for s in data['shapes'])
-                # Only replace data after everything is loaded.
-                self.shapes = shapes
-                self.imagePath = imagePath
-                self.imageData = imageData
-                self.lineColor = lineColor
-                self.fillColor = fillColor
-        except Exception, e:
-            raise LabelFileError(e)
+	def load(self, filename):
+		try:
+			with open(filename, 'rb') as f:
+				data = json.load(f)
+				imagePath = data['imagePath']
+				imageData = b64decode(data['imageData'])
+				lineColor = data['lineColor']
+				fillColor = data['fillColor']
+				shapes = ((s['label'], s['points'], s['line_color'], s['fill_color'])\
+						for s in data['shapes'])
+				# Only replace data after everything is loaded.
+				self.shapes = shapes
+				self.imagePath = imagePath
+				self.imageData = imageData
+				self.lineColor = lineColor
+				self.fillColor = fillColor
+		except Exception, e:
+			raise LabelFileError(e)
 
-    def save(self, filename, shapes, imagePath, imageData,
-            lineColor=None, fillColor=None):
-        try:
-            with open(filename, 'wb') as f:
-                json.dump(dict(
-                    shapes=shapes,
-                    lineColor=lineColor, fillColor=fillColor,
-                    imagePath=imagePath,
-                    imageData=b64encode(imageData)),
-                    f, ensure_ascii=True, indent=2)
+	def save(self, filename, shapes, imagePath, imageData,
+			lineColor=None, fillColor=None):
+		try:
+			with open(filename, 'wb') as f:
+				json.dump(dict(
+					shapes=shapes,
+					lineColor=lineColor, fillColor=fillColor,
+					imagePath=imagePath,
+					imageData=b64encode(imageData)),
+					f, ensure_ascii=True, indent=2)
 
-            # Also export the mask image
-            shape = QImage.fromData(imageData).size()
-            M = shape.height()
-            N = shape.width()
-            
-            maskfilepath = filename[:-4] + "_mask.png"
-            mask = np.zeros((M,N))
-            
-            for i in range(M):
-                for j in range(N):
-                    for shape in shapes:
-                        if self.point_in_poly(j,i,shape['points']):
-                            mask[i][j] = int(shape['label'])
-            
-            print np.unique(mask)
-            # mask = cv2.flip(mask, 0)
+			# Also export the mask image
+			shape = QImage.fromData(imageData).size()
+			M = shape.height()
+			N = shape.width()
+			
+			maskfilepath = filename[:-4] + "_truth.csv"
+			fullmask = np.zeros((M,N))
+			
+			for shape in shapes:
+				mask = self.polygon(M, N, shape['points'])
+				print mask
+				fullmask[mask] = int(shape['label'])
+			
+			np.savetxt(str(maskfilepath), fullmask, fmt = "%d")
 
-            cv2.imwrite(str(maskfilepath), mask*20)
+		except Exception, e:
+			raise LabelFileError(e)
 
-        except Exception, e:
-            raise LabelFileError(e)
+	def polygon(self, M, N, poly):
+		out = np.zeros((M, N)). astype(bool)
 
-    def point_in_poly(self, x, y, poly):
-        # point in polygon algorithm
-        # Ray casting method
 		n = len(poly)
-		inside = False
+		for i in range(M):
+			intersection_x = i
+			intersection_y = []
 
-		p1x,p1y = poly[0]
-		for i in range(n+1):
-			p2x,p2y = poly[i % n]
-			if y > min(p1y,p2y):
-				if y <= max(p1y,p2y):
-					if x <= max(p1x,p2x):
-						if p1y != p2y:
-							xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-						if p1x == p2x or x <= xints:
-							inside = not inside
-			p1x,p1y = p2x,p2y
+			# check through all edges
+			for edge in range(n + 1):
+				v1_x, v1_y = poly[edge % n]
+				v2_x, v2_y = poly[(edge + 1) % n]
 
-		return inside
+				A1, B1, C1 = self.getABC(v1_x, v1_y, v2_x, v2_y)
+				A2 = 1
+				B2 = 0
+				C2 = i
 
-    @staticmethod
-    def isLabelFile(filename):
-        return os.path.splitext(filename)[1].lower() == LabelFile.suffix
+				# find intersection
+				det = A1*B2 - A2*B1
+				if (det != 0):
+					tmp = (A1 * C2 - A2 * C1)/det
+					if tmp >= min(v1_y, v2_y) and tmp <= max(v1_y, v2_y):
+						intersection_y.append(tmp)
+
+			intersection_y = list(set(intersection_y))
+
+			if len(intersection_y) == 1:
+				intersection_y.append(intersection_y[0])
+
+			for k in range(1, len(intersection_y), 2):
+				out[intersection_x, intersection_y[k - 1]:intersection_y[k]] = True
+
+		return out
+
+	def getABC(self, x1, y1, x2, y2):
+		A = y2 - y1
+		B = x1 - x2
+		C = A*x1 + B*y1
+		return (A, B, C)
+
+	@staticmethod
+	def isLabelFile(filename):
+		return os.path.splitext(filename)[1].lower() == LabelFile.suffix
 
